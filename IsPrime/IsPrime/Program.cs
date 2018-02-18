23b505236f4f2aa259d18dabe96dad91b58313a8
Emil Hopf <35430869+jaemil@users.noreply.github.com>
2018-02-18 14:56:28 +0100

    Add files via upload

--- IsPrime/IsPrime/Program.cs
+++ IsPrime/IsPrime/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IsPrime
+namespace Primzahl
 {
     class Program
     {
+        //Algorithm that indicates if a number is a prime
+        //http://www.jaemil.de
+
         static void Main(string[] args)
         {
+            int number;
+            bool prime = true;
+
+            Console.WriteLine("Enter a positive number:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < number; i++)                //i = 2, because 1 and 2 are prime.
+            {
+                if (number % i == 0)                        //every number will be checked except i >= number.
+                {
+                    prime = false;
+                }
+            }
+
+            Console.WriteLine("Is this Number ({0}) a prime? \n{1}", number, prime);
         }
     }
 }
